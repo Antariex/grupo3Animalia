141a016842b450b85a994e4fa68e7b1599039c8a
MaruBruno <marubruno@gmail.com>
2022-08-08 20:49:58 -0300

    arreglo js.register

--- public/js/js_register.js
+++ public/js/js_register.js
@@ -4,23 +4,59 @@ window.addEventListener("load", function(){
     formulario.addEventListener("submit", function(e) {
         let errores = [];
 
-        let campoNombreYApellido = document.querySelector("input.nombreyapellido");
 
+        let campoNombreYApellido = document.querySelector("input.nombreyapellido");
         if (campoNombreYApellido.value == "") {
-            errores.push("El campo Nombre y Apellido deber estar completo");
+            errores.push("El campo Nombre y Apellido debe estar completo");
         } 
 
-        let user = document.querySelector("input.user");
 
+        let user = document.querySelector("input.user");
         if (user.value == "") {
-            errores.push("El campo Nombre de usuario deber estar completo")
+            errores.push("El campo Nombre de usuari@ debe estar completo")
         } else if (user.value.length < 6) {
-            errores.push("El campo debe tener al menos 6 carácteres");
+            errores.push("Nombre de usuari@ debe tener al menos 6 carácteres");
         }
 
+
+        let email = document.querySelector("input.email");
+        if (email.value == "") {
+            errores.push("El campo Email debe estar completo")
+        }
+
+
+        let password = document.querySelector("input.password");
+        if (password.value == "") {
+            errores.push("El campo Contraseña debe estar completo")
+        } else if (password.value.length < 8) {
+            errores.push("La Contraseña debe tener al menos 8 carácteres");
+        }
+
+
+        /*let avatar = document.querySelector("input.avatar");
+        if(avatar == null) {
+             errores.push('No ha seleccionado una imagen')
+        } else {
+		// recuperamos la extensión del archivo
+		let ext = fileName.split('.').pop();
+		// Convertimos en minúscula porque la extensión del archivo puede estar en mayúscula
+		ext = ext.toLowerCase();
+    	// console.log(ext);
+		switch (ext) {
+			case 'jpg':
+			case 'jpeg':
+			case 'png':
+			case 'gif': break;
+			default:
+				alert('El archivo no tiene la extensión adecuada');
+				this.value = ''; // reset del valor
+				this.files[0].name = '';
+		}
+	}*/
+
+        
         if (errores.length > 0) {
             e.preventDefault ();
-
         let ulErrores = document.querySelector ("div.errores ul");
         for (let i = 0; i < errores.length; i++) {
             ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
