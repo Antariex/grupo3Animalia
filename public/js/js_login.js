f02e701fe7bb431c5e44edba5d9d70585fc70005
MaruBruno <marubruno@gmail.com>
2022-08-14 08:03:12 -0300

    actualiza validaciones

--- public/js/js_login.js
+++ public/js/js_login.js
@@ -1,1 +1,32 @@
-console.log ("Probando");
+window.addEventListener("load", function(){
+    let formulario = document.querySelector ("form.login");
+
+    formulario.addEventListener("submit", function(e) {
+        let errores = [];
+
+
+
+        let usuario = document.querySelector("input.usuario");
+        if (usuario.value == "") {
+            errores.push("El Email ingresado no se encuentra registrado")
+        }
+/* el email debe estar en la base de datos!!!! averiguar */ 
+
+
+        let password = document.querySelector("input.password");
+        if (password.value == "") {
+            errores.push("La Contraseña es incorrecta")
+        }
+/* el email debe coincidir con el registrado en la base de datos*/
+
+
+        if (errores.length > 0) {
+            e.preventDefault ();
+        let ulErrores = document.querySelector ("div.errores ul");
+        for (let i = 0; i < errores.length; i++) {
+            ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
+        }
+    }
+
+          });
+})
--- public/js/js_register.js
+++ public/js/js_register.js
@@ -8,7 +8,7 @@ window.addEventListener("load", function(){
         let campoNombreYApellido = document.querySelector("input.nombreyapellido");
         if (campoNombreYApellido.value == "") {
             errores.push("El campo Nombre y Apellido debe estar completo");
-        } 
+        }
 
 
         let user = document.querySelector("input.user");
@@ -22,38 +22,26 @@ window.addEventListener("load", function(){
         let email = document.querySelector("input.email");
         if (email.value == "") {
             errores.push("El campo Email debe estar completo")
-        }
+        }  else if (email.value !== /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) {
+            errores.push ("No es un formato de email válido")
+        } 
 
 
         let password = document.querySelector("input.password");
         if (password.value == "") {
             errores.push("El campo Contraseña debe estar completo")
         } else if (password.value.length < 8) {
-            errores.push("La Contraseña debe tener al menos 8 carácteres");
+            errores.push("La Contraseña debe tener al menos 8 carácteres")
         }
 
 
-        /*let avatar = document.querySelector("input.avatar");
-        if(avatar == null) {
-             errores.push('No ha seleccionado una imagen')
-        } else {
-		// recuperamos la extensión del archivo
-		let ext = fileName.split('.').pop();
-		// Convertimos en minúscula porque la extensión del archivo puede estar en mayúscula
-		ext = ext.toLowerCase();
-    	// console.log(ext);
-		switch (ext) {
-			case 'jpg':
-			case 'jpeg':
-			case 'png':
-			case 'gif': break;
-			default:
-				alert('El archivo no tiene la extensión adecuada');
-				this.value = ''; // reset del valor
-				this.files[0].name = '';
-		}
-	}*/
-
+        let confirmPassword = document.querySelector("input.confirmPassword");
+        if (confirmPassword.value == "") {
+            errores.push("Por favor confirme su Contraseña")
+        } else if (confirmPassword !== password) {
+            errores.push("Las Contraseñas no coinciden")
+        }
+    
         
         if (errores.length > 0) {
             e.preventDefault ();
