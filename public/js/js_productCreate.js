5808715b70b2bb198693dddb7cd9ba5fcb3a10c1
MaruBruno <marubruno@gmail.com>
2022-08-15 19:28:12 -0300

    actualiza validacion

--- public/js/js_productCreate.js
+++ public/js/js_productCreate.js
@@ -1,11 +1,41 @@
 window.onload = function(){
     
 const form = document.querySelector(".form_creacion")
+const errores= []
+const form_element= document.querySelectorAll(".form_element")
+
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    form.querySelectorAll(".form_element").forEach(input=> console.log("input: ", input))
     
+    let emptyElements = []
+
+    form_element.forEach(input=> {
+        if (input.value === "") {
+            emptyElements.push(input)
+        }
     })
-    
+        if (emptyElements.length > 0) {
+            console.log ("no send form")
+        e.preventDefault()
+
+    }
+ 
+    let nombre = document.querySelector("form_element nombre");
+    if (nombre.value == "") {
+        errores.push("El campo Nombre del producto debe estar completo")
+    } else if (nombre.value.length < 6) {
+        errores.push("Nombre de del producto debe tener al menos 6 carácteres");
+    }
+
+
+    if (errores.length > 0) {
+        e.preventDefault ();
+    let ulErrores = document.querySelector ("ul");
+    for (let i = 0; i < errores.length; i++) {
+        ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
+    }
+}
+
+})
+
 }
