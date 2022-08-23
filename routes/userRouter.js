db3c2fe38d2a5e0c98152522478e3c7e4dd1d308
FernandoMig <fcmiguez@yahoo.com>
2022-08-23 17:20:11 -0300

    adecuación de datos menú Subcategoría y categoría

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController')
+const DBuserController = require('../controllers/DBuserController')
 const multer = require('multer');
 const path = require('path');
 const loginValidations = require('../middlewares/loginValidations');
@@ -22,19 +22,19 @@ const storage = multer.diskStorage({
   const upload = multer({ storage });
 
   /*Ruta del Login*/ //
-router.get('/login', userController.login);
+router.get('/login', DBuserController.login);
 //router.post('./users/login', userController.loginValidations);
 
 /*Ruta del Logout */ //HAY QUE AGREGAR COMO 2DO PARAMETRO EL GUESTMIDDLEWARE
-router.get('/logout', userController.logout)
+//router.get('/logout', DBuserController.logout)
 
 /* Register */
-router.get('/register', userController.registro);
-router.post('/register/confirm', upload.single('avatar'), validations, userController.create);
+//router.get('/register', DBuserController.registro);
+//router.post('/register/confirm', upload.single('avatar'), validations, DBuserController.create);
 /*hay que crear vista de profile a futuro*/
 
 
 
-router.get('/cart', userController.carrito);
+//router.get('/cart', DBuserController.carrito);
 
 module.exports = router;
