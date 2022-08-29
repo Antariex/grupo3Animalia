8e2a2ea2237515ad1ac3c5327a5bf980b3d9cf5f
FernandoMig <fcmiguez@yahoo.com>
2022-08-29 13:14:24 -0300

    fix rutas - adición de avances de DBUserController

--- middlewares/isAdminMiddleware.js
+++ middlewares/isAdminMiddleware.js
@@ -1,4 +1,6 @@
-function isAdmin(req, res, next) {
+//teníamos esto
+
+/*function isAdmin(req, res, next) {
   if(req.session.userLogged && req.session.userLogged.isAdmin == true) {
     next()
   }else {
@@ -7,3 +9,32 @@ function isAdmin(req, res, next) {
 }
 
 module.exports = isAdmin;
+*/
+
+const path = require('path');
+const fs = require('fs');
+const db = require("../database/models");
+
+function isAdminMiddleware(req, res, next) {
+    
+    if (req.cookies.remember != undefined && req.session.userLogged == undefined ) {
+        db.User.findOne({
+            where: {
+                email: req.cookies.remember
+            }
+        })
+        .then(function(userInDB){
+        if(userInDB){
+            var userLogged = userInDB;
+                
+        }
+        if(req.session && req.session.userLogged){
+        req.session.userLogged = userLogged;
+        res.locals.userLogged = req.session.userLogged;
+        };
+    }); 
+};
+next()
+}
+
+module.exports = isAdminMiddleware;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -13,7 +13,6 @@ const loginValidationsMiddleware = require('../middlewares/loginValidationsMiddl
 const registerValidationsMiddleware = require('../middlewares/registerValidationsMiddleware');
 const userLoggedMiddleware = require('../middlewares/userLoggedMiddleware');
 
-console.log("register")
 /* Config del Multer */
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -30,26 +29,20 @@ const storage = multer.diskStorage({
   // #####CRUD Users##############
   
   /*Ruta del Login*/
-  router.get('/login', /*guestMiddleware,*/ DBUserController.login);
-//router.post('./users/login', /*upload.single('avatar'), registerValidationsMiddleware,*/ DBUserController.loginValidation);
-
-/*authMiddleware, falla
-guestMiddleware, sin falla
-isAdminMiddleware, falla
-registerValidationsMiddleware, sin falla
-userLoggedMiddleware, sin falla */
+router.get('/login', guestMiddleware, DBUserController.login);
+router.post('/login', upload.single('avatar'), registerValidationsMiddleware, DBUserController.login);
 
 //router.post('./users/login',loginValidationsMiddleware, DBuserController.loginValidation);
 
 /*Ruta del Logout */ //HAY QUE AGREGAR COMO 2DO PARAMETRO EL GUESTMIDDLEWARE
-//router.get('/logout', DBuserController.logout)
+router.get('/logout', DBUserController.logout)
 
 /* Register */
 router.get('/register', DBUserController.registro);
 router.post('/register', upload.single('avatar'), /*registerValidationsMiddleware,*/ DBUserController.create);
 
 /*hay que crear vista de profile a futuro*/
-
+//router.get("/userProfile", userController.profile)
 
 router.get('/cart', DBUserController.carrito);
 
